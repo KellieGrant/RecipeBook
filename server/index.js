9a85c576177e0707f34a46c143543f52eaa92efc
KeltaDawn <kellieleagrant@gmail.com>
2026-02-28 05:40:07 -0500

    fixed minor express version change

--- server/index.js
+++ server/index.js
@@ -231,7 +231,7 @@ app.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
    const distPath = path.join(__dirname, '..', 'dist');
    app.use(express.static(distPath));
-   app.get('*', (req, res) => {
+   app.get('/*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
    });
 } else {
